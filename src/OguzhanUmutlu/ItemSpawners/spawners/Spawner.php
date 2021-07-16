da4cb81174b2c20aaf5f509ca8e4d5197cb1daaa
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-07-16 15:40:03 +0300

    fix

--- src/OguzhanUmutlu/ItemSpawners/spawners/Spawner.php
+++ src/OguzhanUmutlu/ItemSpawners/spawners/Spawner.php
@@ -4,6 +4,7 @@ namespace OguzhanUmutlu\ItemSpawners\spawners;
 
 use OguzhanUmutlu\ItemSpawners\tasks\BlockTask;
 use OguzhanUmutlu\ItemSpawners\costs\Cost;
+use OguzhanUmutlu\ItemSpawners\ItemSpawners;
 use pocketmine\block\Block;
 use pocketmine\item\Item;
 use pocketmine\level\Level;
